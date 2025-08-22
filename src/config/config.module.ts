09de6c524fb16b1d2b55396ecf92c97a9e28e6d9
UlisesHdzOne <ulisesdejesusk9999@gmail.com>
2025-08-22 12:11:13 -0600

    chore(config): refactor de configuración y limpieza de JWT
    - Eliminación de jwt.constants.ts y uso de ConfigModule para variables de entorno
    
    - Actualización de package.json y package-lock.json para mejorar scripts y dependencias
    
    - Refactor de app.module.ts y main.ts para usar ConfigService y valores por defecto de entorno
    
    - Creación de módulo config para centralizar la configuración y validación con Joi

---
+++ src/config/config.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      isGlobal: true, // permite usarlo en cualquier módulo sin importarlo otra vez
+      envFilePath: '.env', // opcional, si quieres usar otro archivo
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        JWT_SECRET: Joi.string().required(),
+        DATABASE_URL: Joi.string().uri().required(),
+        JWT_EXPIRES_IN: Joi.string().default('1h'),
+      }),
+      validationOptions: {
+        abortEarly: true,
+      },
+    }),
+  ],
+  exports: [NestConfigModule],
+})
+export class ConfigModule {}
