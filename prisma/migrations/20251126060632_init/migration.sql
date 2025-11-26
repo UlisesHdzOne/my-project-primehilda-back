505ccd0c459bed3d6919faf6c0aba54e3626f72d
UlisesHdzOne <ulisesdejesusk9999@gmail.com>
2025-11-26 00:08:53 -0600

    feat(project): configurar Prisma y entorno de base de datos
    - Agrega prisma.config.ts
    
    - Agrega carpeta prisma con schema y migraciones
    
    - Agrega docker-compose para PostgreSQL
    
    - Actualiza .gitignore para nuevas rutas
    
    - Actualiza package.json y package-lock.json para dependencias de Prisma

---
+++ prisma/migrations/20251126060632_init/migration.sql
@@ -0,0 +1,53 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Sala" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "capacity" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Sala_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Reserva" (
+    "id" SERIAL NOT NULL,
+    "fecha" TIMESTAMP(3) NOT NULL,
+    "horaInicio" TEXT NOT NULL,
+    "horaFin" TEXT NOT NULL,
+    "motivo" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "salaId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Reserva_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Sala_name_key" ON "Sala"("name");
+
+-- CreateIndex
+CREATE INDEX "Reserva_salaId_fecha_idx" ON "Reserva"("salaId", "fecha");
+
+-- AddForeignKey
+ALTER TABLE "Reserva" ADD CONSTRAINT "Reserva_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Reserva" ADD CONSTRAINT "Reserva_salaId_fkey" FOREIGN KEY ("salaId") REFERENCES "Sala"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
